4e8b84609b3637513e6c32d7333e3daa22dca3fe
Scott Addie <scott.addie@microsoft.com>
2018-02-18 22:51:05 -0600

    Add test project

---
+++ TagHelperSuite.Tests/LinkButtonTagHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TagHelperSuite.Tests
+{
+    public class LinkButtonTagHelperTests
+    {
+        [Fact]
+        public void ProcessGeneratesExpectedRootElement()
+        {
+            // ==== Arrange ====
+            const string ROOT_HTML_ELEMENT_TAG = "a";
+
+            var context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+
+            var output = new TagHelperOutput(
+                ROOT_HTML_ELEMENT_TAG,
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+            var tagHelper = new LinkButtonTagHelper();
+
+            // ==== Act ====
+            tagHelper.Process(context, output);
+
+            // ==== Assert ====
+            // Compare root HTML elements
+            Assert.Equal(ROOT_HTML_ELEMENT_TAG, output.TagName);
+        }
+    }
+}
